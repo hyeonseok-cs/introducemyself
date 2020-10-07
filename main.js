770183405601281de3174e94c506b79885256d9a
hyeonseok-cs <72328454+hyeonseok-cs@users.noreply.github.com>
2020-10-07 15:31:58 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const toggleBtn = document.querySelector(".navbar__toogleBtn");
+const toggleBtn = document.querySelector(".navbar__menu");
+const toggleBtn = document.querySelector(".navbar__icons");
+
+toggleBtn.addEventListener("click", () => {
+    menu.classList.toogle("active");
+    icons.classList.toogle("active");
+});
+const toogleBtm = document.querySelector
